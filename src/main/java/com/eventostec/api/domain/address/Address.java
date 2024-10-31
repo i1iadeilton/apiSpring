731203913a54a91711c85ea40bde29bbdba5fd54
i1iadeilton <i1iadeilton@gmail.com>
2024-10-31 09:31:59 -0300

    primeiro commit

---
+++ src/main/java/com/eventostec/api/domain/address/Address.java
@@ -0,0 +1,29 @@
+package com.eventostec.api.domain.address;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Address adress;
+}
